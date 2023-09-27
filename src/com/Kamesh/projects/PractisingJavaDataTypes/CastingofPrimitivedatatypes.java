f886b7bd3ee97963e5a1e8d04e084499fb088f00
Kamesh <kameshdangeti48@gmail.com>
2023-09-27 14:29:06 +0530

    practising of java datatypes casting primitive datatypes has been commited

---
+++ src/com/Kamesh/projects/PractisingJavaDataTypes/CastingofPrimitivedatatypes.java
@@ -0,0 +1,17 @@
+package com.Kamesh.projects.PractisingJavaDataTypes;
+
+public class CastingofPrimitivedatatypes
+{
+public static void main(String[] args)
+{
+	int Integer=10;             
+	double Double=Integer;
+	System.out.println("Integer:"+Integer);
+	System.out.println("Casting to Double :"+Double);
+	
+	double D=9.0;
+	int i=(int)D;
+	System.out.println("Double :"+D);
+	System.out.println("casting to Integer :"+i);
+}
+}
---
+++ src/com/Kamesh/projects/PractisingJavaDataTypes/CastingofPrimitivedatatypes3.java
@@ -0,0 +1,17 @@
+package com.Kamesh.projects.PractisingJavaDataTypes;
+
+public class CastingofPrimitivedatatypes3 
+{
+ public static void main(String[] args) 
+ {
+   int integer=30;
+   double Double =integer;
+   System.out.println("Integer :"+integer);
+   System.out.println("Casted Double :"+Double);
+   
+   double d=40.0;
+   int i=(int)d;
+   System.out.println("Double:"+d);
+   System.out.println("Casted int:"+i);
+}
+}
