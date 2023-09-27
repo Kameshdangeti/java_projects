d6ef67ceea881498af354c02d3963ad13e4b731e
Kamesh <kameshdangeti48@gmail.com>
2023-09-27 17:28:07 +0530

    Practising of operators and decision Constructs 2 equal methods has been commited

---
+++ src/com/Kamesh/projects/PractisingOperatorsAndDecisionConstructors/Creating2Equals4.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.PractisingOperatorsAndDecisionConstructors;
+
+public class Creating2Equals4 
+{
+  public static void main(String[] args)
+  {
+	String s1="Kamesh";
+	String s2="Lucky";
+	String s3="Kamesh";
+	System.out.println("Statement is:"+(s1==s3));
+	System.out.println("Statement is:"+(s2==s3));
+	System.out.println("Statement is:"+s1.equals(s2));
+	System.out.println("Statement is:"+s2.equals(s3));
+  }
+}
---
+++ src/com/Kamesh/projects/PractisingOperatorsAndDecisionConstructors/Creating2Equals5.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.PractisingOperatorsAndDecisionConstructors;
+
+public class Creating2Equals5 
+{
+	public static void main(String[] args) 
+	{
+		String s1="Kamesh";
+		String s2="Lucky";
+		String s3="Kamesh";
+		System.out.println("Statement is:"+(s1==s2));
+		System.out.println("Statement is:"+(s1==s3));
+		System.out.println("Statement is:"+s1.equals(s2));
+		System.out.println("Statement is:"+s1.equals(s3));
+	}
+}
