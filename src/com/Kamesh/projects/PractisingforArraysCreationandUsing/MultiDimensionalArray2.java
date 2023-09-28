25310e15e8f13dece9cb31ca57f100e9656d9b4a
Kamesh <kameshdangeti48@gmail.com>
2023-09-28 14:52:30 +0530

    practsing of arrays usage and creation of multidimensional array has been commited

---
+++ src/com/Kamesh/projects/PractisingforArraysCreationandUsing/MultiDimensionalArray2.java
@@ -0,0 +1,22 @@
+package com.Kamesh.projects.PractisingforArraysCreationandUsing;
+
+public class MultiDimensionalArray2 
+{
+	public static void main(String[] args)
+	{
+		int [][][]a= {{{10,20,30},{40,50,60}},{{70,80,90},{100,110,120}}};
+		System.out.println("Multi dimensional array");
+		for(int [][]b:a)
+		{
+			for(int []c:b)
+			{
+				for(int d:c)
+				{
+					System.out.println(d);
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+}
