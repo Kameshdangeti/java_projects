c1e5bf022a66de6a0d75c0e57fc0804717c3d7d7
Kamesh <kameshdangeti48@gmail.com>
2023-09-28 16:12:47 +0530

    practsing of looping constructors in do while loop has been commited

---
+++ src/com/Kamesh/projects/practsingLoopConstructor/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+public class DoWhileLoop 
+{
+	public static void main(String[] args)
+	{
+		int i=1;
+	    do
+	    {
+	    	System.out.println(i);
+			i++;
+		}
+	    while (i<=10);
+	}
+}
---
+++ src/com/Kamesh/projects/practsingLoopConstructor/DoWhileLoop4.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+public class DoWhileLoop4
+{
+	public static void main(String[] args) 
+	{
+		int i=1;
+		do 
+		{
+			System.out.println(i);
+			i++;
+		} 
+		while (i<=40);
+	}
+}
