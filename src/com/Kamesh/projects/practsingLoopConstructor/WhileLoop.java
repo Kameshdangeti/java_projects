5e12a0d7c07b554bf98f3d3916af5b7daac9fdda
Kamesh <kameshdangeti48@gmail.com>
2023-09-28 15:55:42 +0530

    practising of loop contructors in while loop has been commited

---
+++ src/com/Kamesh/projects/practsingLoopConstructor/WhileLoop.java
@@ -0,0 +1,24 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+public class WhileLoop 
+{
+	public static void main(String[] args) 
+	{
+		int n=23456;
+		int pro=1;
+		while(n!=0)
+		{
+		   int ld=n%10;
+		   pro=pro*ld;
+		   n=n/10;
+		}
+		if (pro%2==0)
+		{
+			System.out.println("Product of digit is even");
+		}
+		else
+		{
+			System.out.println("Product of digits is odd");
+		}
+	}
+}
---
+++ src/com/Kamesh/projects/practsingLoopConstructor/WhileLoop5.java
@@ -0,0 +1,24 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+public class WhileLoop5 
+{
+	public static void main(String[] args)
+	{
+		int n=18736;
+		int pro=1;
+		while(n!=0)
+		{
+			int ld=n%10;
+			pro=ld*pro;
+	        n=n/10;
+		}
+		if (pro%2==0) 
+		{
+		  System.out.println("product of digit is even");	
+		}
+		else
+		{
+			System.out.println("product of digit is odd");
+		}
+	}
+}
