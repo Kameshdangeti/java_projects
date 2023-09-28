f6493bd5cdb00ba2dbc97f26af4d147b01a26191
Kamesh <kameshdangeti48@gmail.com>
2023-09-28 18:01:38 +0530

    practising of looping constructors in Enhanced loop has been commited

---
+++ src/com/Kamesh/projects/practsingLoopConstructor/EnhancedForLoop.java
@@ -0,0 +1,26 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+public class EnhancedForLoop 
+{
+	public static void main(String[] args)
+	{
+		LinkedHashSet<Object> l=new LinkedHashSet();
+		l.add("Name : Kamesh");
+		l.add("Age : 21");
+		l.add("gender : Male");
+		
+		Object [] o=l.toArray();
+		
+	    for (int i = 2; i < o.length-1; i++)
+	    {
+			System.out.println(o[i]);
+		}	
+	    for (Object  details : l) 
+	    {
+			System.out.println(details);
+		}
+	}
+}
---
+++ src/com/Kamesh/projects/practsingLoopConstructor/EnhancedForLoop3.java
@@ -0,0 +1,25 @@
+package com.Kamesh.projects.practsingLoopConstructor;
+
+import java.util.LinkedHashSet;
+
+public class EnhancedForLoop3 
+{
+	public static void main(String[] args)
+	{
+		LinkedHashSet<Object> l=new LinkedHashSet();
+		l.add("Name : Kamesh");
+		l.add("Age : 21");
+		l.add("gender : Male");
+		
+		Object [] o=l.toArray();
+		
+	    for (int i = 2; i < o.length-1; i++)
+	    {
+			System.out.println(o[i]);
+		}	
+	    for (Object  details : l) 
+	    {
+			System.out.println(details);
+		}
+	}
+}
