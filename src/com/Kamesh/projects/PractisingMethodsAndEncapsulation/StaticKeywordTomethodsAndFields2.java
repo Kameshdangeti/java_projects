31e6e76d1ad1d4db7a04b85c2ca1e856b99e3d7b
Kamesh <kameshdangeti48@gmail.com>
2023-09-29 14:09:12 +0530

    practising of methods and encapsulation in static keywords to methods and fields

---
+++ src/com/Kamesh/projects/PractisingMethodsAndEncapsulation/StaticKeywordTomethodsAndFields2.java
@@ -0,0 +1,32 @@
+package com.Kamesh.projects.PractisingMethodsAndEncapsulation;
+
+public class StaticKeywordTomethodsAndFields2 
+{
+	public String Name;
+    public   int rollno;
+	static String clgname;
+	static int counter=0;
+
+	public StaticKeywordTomethodsAndFields2(String name) 
+	{
+		super();
+		this.Name = name;
+		this.rollno = setRollno();
+	}
+	static int setRollno() 
+	{
+		counter++;
+		return counter;
+	}
+
+    static void setclg(String name)
+    {
+    	clgname=name;
+    }
+    public void getPersoninfo()
+	{
+		System.out.println("Person Name :"+this.Name);
+		System.out.println("Person Rollno:"+this.rollno);
+		System.out.println("Person college name:"+clgname);
+	}  
+}
---
+++ src/com/Kamesh/projects/PractisingMethodsAndEncapsulation/StaticKeywordTomethodsAndFieldsMain2.java
@@ -0,0 +1,13 @@
+package com.Kamesh.projects.PractisingMethodsAndEncapsulation;
+
+public class StaticKeywordTomethodsAndFieldsMain2 
+{
+	public static void main(String[] args)
+	{
+	StaticKeywordTomethodsAndFields s1 =new StaticKeywordTomethodsAndFields("Kamesh");
+	StaticKeywordTomethodsAndFields s2 =new StaticKeywordTomethodsAndFields("Lucky");
+	StaticKeywordTomethodsAndFields.setclg("BVCE");
+	s1.getPersoninfo();
+	s2.getPersoninfo();
+	}
+}
