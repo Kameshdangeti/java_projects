e23736ea0553638c27176c29ca913f83ae472e5a
Kamesh <kameshdangeti48@gmail.com>
2023-10-01 18:40:22 +0530

    practising of java Api of ArrayList with multiple metods has been commited

---
+++ src/com/Kamesh/projects/PractisingJavaApi/ArrayListWithMultipleMethods3.java
@@ -0,0 +1,26 @@
+package com.Kamesh.projects.PractisingJavaApi;
+
+import java.util.ArrayList;
+
+public class ArrayListWithMultipleMethods3 
+{
+    public static void main(String[] args) 
+    {
+    	 ArrayList<Object> a=new ArrayList<Object>();
+			a.add('M');
+			a.add("Ramesh");
+			a.add(9);
+			a.add(33.33);
+			a.add(null);
+			a.add(true);
+			System.out.println(a);                     
+			System.out.println(a.contains("Ramesh"));    
+			System.out.println(a.get(2));               
+			System.out.println(a.set(1, 99));          
+			System.out.println(a);                  
+			System.out.println(a.size());              
+			System.out.println(a.isEmpty());           
+			System.out.println(a.remove(2));            
+			a.clear();          	
+	}
+}
---
+++ src/com/Kamesh/projects/PractisingJavaApi/ArrayListWithMultipleMethods5.java
@@ -0,0 +1,26 @@
+package com.Kamesh.projects.PractisingJavaApi;
+
+import java.util.ArrayList;
+
+public class ArrayListWithMultipleMethods5 
+{
+     public static void main(String[] args)
+     {
+    	 ArrayList<Object> a=new ArrayList<Object>();
+			a.add('S');
+			a.add("Kamesh");
+			a.add(9);
+			a.add(55.55);
+			a.add(null);
+			a.add(true);
+			System.out.println(a);                     
+			System.out.println(a.contains("Kamesh"));    
+			System.out.println(a.get(3));               
+			System.out.println(a.set(1, 99));          
+			System.out.println(a);                  
+			System.out.println(a.size());              
+			System.out.println(a.isEmpty());           
+			System.out.println(a.remove(2));            
+			a.clear();   
+	 }
+}
