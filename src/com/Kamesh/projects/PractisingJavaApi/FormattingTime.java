a64154de187c0bfa57fe066d420045896f008a2d
Kamesh <kameshdangeti48@gmail.com>
2023-10-02 15:12:10 +0530

    Practising of java api of formatting time has been commited

---
+++ src/com/Kamesh/projects/PractisingJavaApi/FormattingTime.java
@@ -0,0 +1,22 @@
+package com.Kamesh.projects.PractisingJavaApi;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class FormattingTime 
+{
+    public static void main(String[] args) 
+    {
+		LocalTime time=LocalTime.of(14, 47);
+		
+		System.out.println(time);
+		
+		DateTimeFormatter formatter1 =DateTimeFormatter.ofPattern("hh:mm:ss");
+		DateTimeFormatter formatter2 =DateTimeFormatter.ofPattern("hh:mm");
+		DateTimeFormatter formatter3 =DateTimeFormatter.ofPattern("'Phone call on' h-mm a");
+		
+		System.out.println(time.format(formatter1));
+		System.out.println(time.format(formatter2));
+		System.out.println(time.format(formatter3));
+	}
+}
