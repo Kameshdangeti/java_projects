fc7f7fa830c022cefc5f761b79fab6a4ffa6560a
Kamesh <kameshdangeti48@gmail.com>
2023-10-02 14:37:36 +0530

    Practising the java Api of  Formatting date has been commited

---
+++ src/com/Kamesh/projects/PractisingJavaApi/FormattingDate2.java
@@ -0,0 +1,25 @@
+package com.Kamesh.projects.PractisingJavaApi;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+public class FormattingDate2 
+{
+	public static void main(String[] args) 
+	{
+		 LocalDate date =LocalDate.of(2023, Month.OCTOBER, 02);
+		   
+		   System.out.println(date);
+		   
+		  DateTimeFormatter formatter= DateTimeFormatter.ofPattern("yyyy-MMMM-dd");
+		  
+	      DateTimeFormatter formatter2=DateTimeFormatter.ofPattern("yyyy-MM-dd E D");
+	      
+	      DateTimeFormatter formatter3=DateTimeFormatter.ofPattern("yy-MMM-dd EEEE d");
+	      
+	      System.out.println(date.format(formatter));
+	      System.out.println(date.format(formatter2));
+	      System.out.println(date.format(formatter3));
+	}
+}
