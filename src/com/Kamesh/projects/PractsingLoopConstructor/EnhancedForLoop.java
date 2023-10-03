6740cd60e7d4aae04e3d4b8b1cb961be13f4bbf2
Kamesh <kameshdangeti48@gmail.com>
2023-10-03 17:20:15 +0530

    Package name is changed

--- src/com/Kamesh/projects/PractsingLoopConstructor/EnhancedForLoop.java
+++ src/com/Kamesh/projects/PractsingLoopConstructor/EnhancedForLoop.java
@@ -3,23 +3,19 @@ package com.Kamesh.projects.PractsingLoopConstructor;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 
-public class EnhancedForLoop 
-{
-	public static void main(String[] args)
-	{
-		LinkedHashSet<Object> l=new LinkedHashSet();
+public class EnhancedForLoop {
+	public static void main(String[] args) {
+		LinkedHashSet<Object> l = new LinkedHashSet();
 		l.add("Name : Kamesh");
 		l.add("Age : 21");
 		l.add("gender : Male");
-		
-		Object [] o=l.toArray();
-		
-	    for (int i = 2; i < o.length-1; i++)
-	    {
+
+		Object[] o = l.toArray();
+
+		for (int i = 2; i < o.length - 1; i++) {
 			System.out.println(o[i]);
-		}	
-	    for (Object  details : l) 
-	    {
+		}
+		for (Object details : l) {
 			System.out.println(details);
 		}
 	}
