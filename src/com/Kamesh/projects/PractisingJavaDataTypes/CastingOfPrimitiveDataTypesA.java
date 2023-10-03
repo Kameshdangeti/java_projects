200b3a0fbd6ec18c504bca8b8094f5140215d680
Kamesh <kameshdangeti48@gmail.com>
2023-10-03 17:00:31 +0530

    Fixed some errors with typos

---
+++ src/com/Kamesh/projects/PractisingJavaDataTypes/CastingOfPrimitiveDataTypesA.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.PractisingJavaDataTypes;
+
+public class CastingOfPrimitiveDataTypesA {
+	public static void main(String[] args) {
+		int Integer = 20;
+		double Double = Integer;
+		System.out.println("Integer :" + Integer);
+		System.out.println("Casted to double:" + Double);
+
+		double d = 30.0;
+		int i = (int) d;
+		System.out.println("Double value:" + d);
+		System.out.println("Casted to Int:" + i);
+	}
+}
---
+++ src/com/Kamesh/projects/PractisingJavaDataTypes/CastingOfPrimitiveDataTypesD.java
@@ -0,0 +1,15 @@
+package com.Kamesh.projects.PractisingJavaDataTypes;
+
+public class CastingOfPrimitiveDataTypesD {
+	public static void main(String[] args) {
+		int i = 60;
+		double d = i;
+		System.out.println("Integer:" + i);
+		System.out.println("Casted double:" + d);
+
+		double d1 = 70.0;
+		int i1 = (int) d1;
+		System.out.println("Double:" + d1);
+		System.out.println("Casted Int:" + i1);
+	}
+}
