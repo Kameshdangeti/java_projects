f0b065e176f17dc3e7fc1d8ffe1d2e20c9ed053a
Kamesh <kameshdangeti48@gmail.com>
2023-10-07 15:48:20 +0530

    Practising of java programs has been written and commited

---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/PrintingFormattedOutput.java
@@ -0,0 +1,23 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+import java.util.Scanner;
+
+public class PrintingFormattedOutput 
+{
+	public static void main(String[] args)
+	{
+		 Scanner sc=new Scanner(System.in);
+         for(int i=0;i<3;i++)
+         {
+	         String s1=sc.next();
+	         int x=sc.nextInt();
+	        // System.out.printf("%-15s",s1);
+	        // System.out.printf("%03d",x);
+	        // System.out.println();
+	
+	         System.out.printf("%-15s%03d\n",s1,x);
+          }
+         sc.close();
+	}
+
+}
