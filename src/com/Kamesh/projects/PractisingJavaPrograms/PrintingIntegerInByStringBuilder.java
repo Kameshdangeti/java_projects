22ff1064338f3a2837c5f4aa5d154206a3226235
Kamesh <kameshdangeti48@gmail.com>
2023-10-07 17:12:09 +0530

    Practising of java programs has been commited

---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/PrintingIntegerInByStringBuilder.java
@@ -0,0 +1,27 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+import java.util.Scanner;
+
+public class PrintingIntegerInByStringBuilder 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		 int t=sc.nextInt();
+	        for(int i=0;i<t;i++)
+	        {
+	            int a = sc.nextInt();
+	            int b = sc.nextInt();
+	            int n = sc.nextInt();
+	            StringBuilder output = new StringBuilder();    
+	        for (int j=0; j<n; j++) 
+	        {
+	             a += b << j; 
+	             output.append(a + " ");   
+	        }
+	            System.out.println(output.toString());
+	        }
+	        sc.close();
+	}
+
+}
