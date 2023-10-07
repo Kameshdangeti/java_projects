18d3a4710b53149c1f3f2f2ec44656e65d3e503f
Kamesh <kameshdangeti48@gmail.com>
2023-10-07 15:02:48 +0530

    Practsing of java programs has been written and commited

---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/ReadinIntegerDoubleString.java
@@ -0,0 +1,23 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+import java.util.Scanner;
+
+public class ReadinIntegerDoubleString
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the integer value :");
+		int i = sc.nextInt();
+		System.out.println("Enter the double value :");
+        double d = sc.nextDouble();
+        sc.nextLine();
+        System.out.println("Enter the String value :");
+        String s = sc.nextLine();
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+		sc.close();
+	}
+
+}
