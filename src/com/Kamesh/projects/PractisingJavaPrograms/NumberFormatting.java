aa498fa4a94dc171a9ce2a97eec86b69bb415ff4
Kamesh <kameshdangeti48@gmail.com>
2023-10-08 12:23:52 +0530

    Creation of number formatting code has been developed and commited

---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/NumberFormatting.java
@@ -0,0 +1,24 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NumberFormatting {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the amount :");
+		double currency = sc.nextDouble();
+		sc.close();
+		NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+		NumberFormat india = new DecimalFormat("###,###.00");
+		NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINESE);
+		NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+		System.out.println("US: " + us.format(currency));
+		System.out.println("India: Rs." + india.format(currency));
+		System.out.println("China: " + china.format(currency));
+		System.out.println("France: " + france.format(currency));
+	}
+
+}
