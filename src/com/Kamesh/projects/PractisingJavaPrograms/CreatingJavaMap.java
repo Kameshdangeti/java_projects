422bcb2ff3ca4be8553ba05dc2ffaf0b267dab46
Kamesh <kameshdangeti48@gmail.com>
2023-10-09 20:41:33 +0530

    Creating java Map has been commited and pushed

--- src/com/Kamesh/projects/PractisingJavaPrograms/CreatingJavaMap.java
+++ src/com/Kamesh/projects/PractisingJavaPrograms/CreatingJavaMap.java
@@ -3,17 +3,14 @@ package com.Kamesh.projects.PractisingJavaPrograms;
 import java.util.HashMap;
 import java.util.Scanner;
 
-public class CreatingJavaMap 
-{
-	public static void main(String[] args)
-	{
+public class CreatingJavaMap {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		HashMap<String, Integer> phbook = new HashMap<String, Integer>();
 		System.out.println("Enter the details:");
 		int n = sc.nextInt();
 		sc.nextLine();
-		for (int i = 0; i < n; i++) 
-		{
+		for (int i = 0; i < n; i++) {
 			System.out.println("Enter the name");
 			String name = sc.nextLine();
 			System.out.println("Enter the phno");
@@ -21,15 +18,11 @@ public class CreatingJavaMap
 			sc.nextLine();
 			phbook.put(name, phone);
 		}
-		while (sc.hasNext()) 
-		{
+		while (sc.hasNext()) {
 			String s = sc.nextLine();
-			if (phbook.containsKey(s))
-			{
+			if (phbook.containsKey(s)) {
 				System.out.println(s + "=" + phbook.get(s));
-			}
-			else
-			{
+			} else {
 				System.out.println("Not found");
 			}
 		}
