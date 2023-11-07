9e1c74b82b8ce7e6a9fee88ca3316be4547f2138
Kamesh <kameshdangeti48@gmail.com>
2023-11-07 13:12:04 +0530

    Printing of java inheritance has been developed and commited

---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/Animal.java
@@ -0,0 +1,8 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+public class Animal 
+{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
---
+++ src/com/Kamesh/projects/PractisingJavaPrograms/PrintingJavaInheritanceMethod.java
@@ -0,0 +1,12 @@
+package com.Kamesh.projects.PractisingJavaPrograms;
+
+public class PrintingJavaInheritanceMethod 
+{
+	  public static void main(String args[]){
+
+		  Bird bird = new Bird();
+		  bird.walk();
+		  bird.fly();
+	      bird.sing();
+	  }
+}
